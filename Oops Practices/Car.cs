286cfc090fd040ac02e23fad759d3b9b2b06912d
maha <mmahaece07@gmail.com>
2022-01-02 18:24:04 +0530

    Initial Commit

---
+++ Oops Practices/Car.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oops_Practices
+{
+    class Car
+    {
+    }
+}
---
+++ Oops Practices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oops_Practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
